5d837b2b553060927b1d4b58103c0e78fd37afec
Isaac Falkenstine <isaac@falkenstine.com>
2019-01-29 13:42:19 -0700

    Created migrations for our database.
    Created foods, meals and meal_food tables migrations in our databse

---
+++ db/migrations/20190129132728_initial.js
@@ -0,0 +1,41 @@
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.createTable('foods', function(table) {
+      table.increments('id').primary();
+      table.string('name');
+      table.string('calories');
+
+      table.timestamps(true, true);
+    }),
+
+    knex.schema.createTable('meals', function(table) {
+      table.increments('id').primary();
+      table.string('name');
+
+      table.timestamps(true, true);
+    }),
+
+    knex.schema.createTable('meal_foods', function(table) {
+      table.increments('id').primary();
+
+      table.integer('food_id').unsigned()
+      table.foreign('food_id')
+        .references('foods.id');
+
+      table.integer('meal_id').unsigned()
+      table.foreign('meal_id')
+        .references('meals.id');
+
+      table.timestamps(true, true);
+    })
+  ])
+};
+
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.dropTable('foods'),
+    knex.schema.dropTable('meals'),
+    knex.schema.dropTable('meal_foods')
+  ]);
+}
