6a2554537d1d577cf1a598a1445a63dae95e3513
Preston Jarnagin <prstnjarnagin@gmail.com>
2019-01-30 11:46:32 -0700

    Write some endpoint tests

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -31,4 +31,70 @@ describe('Client Routes', () => {
 
 describe('API Routes', () => {
 
+  it('GET api/v1/foods', done => {
+    chai.request(server)
+    .get('/api/v1/foods')
+    .end((err, response) => {
+      response.should.have.status(200);
+      response.should.be.json;
+      response.body.should.be.a('array');
+      response.body[0].should.have.property('name');
+      response.body[0].name.should.equal('Brownie');
+      response.body[0].should.have.property('calories');
+      response.body[0].calories.should.equal('250');
+      done();
+    });
+  });
+
+  it('GET api/v1/meals', done => {
+    chai.request(server)
+    .get('/api/v1/meals')
+    .end((err, response) => {
+      response.should.have.status(200);
+      response.should.be.json;
+      response.body.should.be.a('array');
+      response.body[0].should.have.property('name');
+      response.body[0].name.should.equal('Dinner');
+      done();
+    });
+  });
+
+  it('GET api/v1/foods/:id', done => {
+    chai.request(server)
+    .get(`/api/v1/foods/`)
+    .end((err, response) => {
+      let foodID = response.body[0].id;
+      chai.request(server)
+      .get(`/api/v1/foods/${foodID}`)
+      .end((err, response) => {
+
+        response.should.have.status(200);
+        response.should.be.json;
+        response.body.should.be.a('array');
+        response.body[0].should.have.property('name');
+        response.body[0].name.should.equal('Brownie');
+        response.body[0].should.have.property('calories');
+        response.body[0].calories.should.equal('250');
+        done();
+      });
+    })
+  });
+
+  it('GET api/v1/meals/:id', done => {
+    chai.request(server)
+    .get(`/api/v1/meals/`)
+    .end((err, response) => {
+      let mealID = response.body[0].id;
+      chai.request(server)
+      .get(`/api/v1/meals/${mealID}`)
+      .end((err, response) => {
+        response.should.have.status(200);
+        response.should.be.json;
+        response.body.should.be.a('array');
+        response.body[0].should.have.property('name');
+        response.body[0].name.should.equal('Dinner');
+        done();
+      });
+    })
+  });
 });
