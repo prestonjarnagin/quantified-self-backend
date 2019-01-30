fac893f74bdb3fc53cd2928c912b79fa5f028b69
Quinn Krug <quinn.krug@gmail.com>
2019-01-29 19:21:15 -0700

    meals endpoint

--- index.js
+++ index.js
@@ -15,7 +15,15 @@ app.get('/', (request, response) => {
   response.send('Hello');
 });
 
-
+app.get('/api/v1/meals', (request, response) => {
+  database('meals').select()
+    .then((meals) => {
+      response.status(200).json(meals);
+    })
+    .catch((error) => {
+      response.status(500).json({ error });
+    });
+});
 
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on ${app.get('port')}.`);
