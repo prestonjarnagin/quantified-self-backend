c202c21c62bacd0bd48cb36a4b3d6be45c99e5f6
Preston Jarnagin <prstnjarnagin@gmail.com>
2019-01-30 11:49:01 -0700

    Rearrange routes test file

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -30,44 +30,11 @@ describe('Client Routes', () => {
 });
 
 describe('API Routes', () => {
-
-  it('GET api/v1/foods', done => {
-    chai.request(server)
-    .get('/api/v1/foods')
-    .end((err, response) => {
-      response.should.have.status(200);
-      response.should.be.json;
-      response.body.should.be.a('array');
-      response.body[0].should.have.property('name');
-      response.body[0].name.should.equal('Brownie');
-      response.body[0].should.have.property('calories');
-      response.body[0].calories.should.equal('250');
-      done();
-    });
-  });
-
-  it('GET api/v1/meals', done => {
-    chai.request(server)
-    .get('/api/v1/meals')
-    .end((err, response) => {
-      response.should.have.status(200);
-      response.should.be.json;
-      response.body.should.be.a('array');
-      response.body[0].should.have.property('name');
-      response.body[0].name.should.equal('Dinner');
-      done();
-    });
-  });
-
-  it('GET api/v1/foods/:id', done => {
-    chai.request(server)
-    .get(`/api/v1/foods/`)
-    .end((err, response) => {
-      let foodID = response.body[0].id;
+  describe('GET', () =>{
+    it('GET api/v1/foods', done => {
       chai.request(server)
-      .get(`/api/v1/foods/${foodID}`)
+      .get('/api/v1/foods')
       .end((err, response) => {
-
         response.should.have.status(200);
         response.should.be.json;
         response.body.should.be.a('array');
@@ -77,16 +44,11 @@ describe('API Routes', () => {
         response.body[0].calories.should.equal('250');
         done();
       });
-    })
-  });
+    });
 
-  it('GET api/v1/meals/:id', done => {
-    chai.request(server)
-    .get(`/api/v1/meals/`)
-    .end((err, response) => {
-      let mealID = response.body[0].id;
+    it('GET api/v1/meals', done => {
       chai.request(server)
-      .get(`/api/v1/meals/${mealID}`)
+      .get('/api/v1/meals')
       .end((err, response) => {
         response.should.have.status(200);
         response.should.be.json;
@@ -95,6 +57,46 @@ describe('API Routes', () => {
         response.body[0].name.should.equal('Dinner');
         done();
       });
+    });
+
+    it('GET api/v1/foods/:id', done => {
+      chai.request(server)
+      .get(`/api/v1/foods/`)
+      .end((err, response) => {
+        let foodID = response.body[0].id;
+        chai.request(server)
+        .get(`/api/v1/foods/${foodID}`)
+        .end((err, response) => {
+
+          response.should.have.status(200);
+          response.should.be.json;
+          response.body.should.be.a('array');
+          response.body[0].should.have.property('name');
+          response.body[0].name.should.equal('Brownie');
+          response.body[0].should.have.property('calories');
+          response.body[0].calories.should.equal('250');
+          done();
+        });
+      })
+    });
+
+    it('GET api/v1/meals/:id', done => {
+      chai.request(server)
+      .get(`/api/v1/meals/`)
+      .end((err, response) => {
+        let mealID = response.body[0].id;
+        chai.request(server)
+        .get(`/api/v1/meals/${mealID}`)
+        .end((err, response) => {
+          response.should.have.status(200);
+          response.should.be.json;
+          response.body.should.be.a('array');
+          response.body[0].should.have.property('name');
+          response.body[0].name.should.equal('Dinner');
+          done();
+        });
+      })
     })
+
   });
 });
