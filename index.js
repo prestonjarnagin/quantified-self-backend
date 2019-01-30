4de19403ed9a61b83abfa39dba726e41a9cd2a66
Isaac Falkenstine <isaac@falkenstine.com>
2019-01-29 22:39:02 -0700

    Building out meal foods endpoint (WIP)

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./knexfile')[environment];
 const database = require('knex')(configuration);
 
+pry = require('pryjs')
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('port', process.env.PORT || 3000);
@@ -21,6 +23,36 @@ app.get('/api/v1/foods', (request, response) => {
       response.status(200).json(foods);
     })
     .catch((error) => {
+      response.status(500).json({ error });
+    });
+});
+
+app.get('/api/v1/foods', (request, response) => {
+  database('foods').select()
+    .then((foods) => {
+      response.status(200).json(foods);
+    })
+    .catch((error) => {
+      response.status(500).json({ error });
+    });
+});
+
+app.get('/api/v1/meals/:meal_id/foods', (request, response) => {
+  database('meal_foods').where('meal_id', request.params.meal_id).pluck('food_id').select()
+    .then(food_id => {
+      eval(pry.it)
+      database('foods').where('id', food_id).select()
+      .then(foods => {
+        if (foods.length) {
+          response.status(200).json(foods);
+        } else {
+          response.status(404).json({
+            error: `Could not find paper with id ${request.params.id}`
+          });
+        }
+      })
+    })
+    .catch(error => {
       response.status(500).json({ error });
     });
 });
