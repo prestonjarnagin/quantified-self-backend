487000a46dac507676abed86dc7980d85b3711d1
Isaac Falkenstine <isaac@falkenstine.com>
2019-01-29 19:05:49 -0700

    Get request for all foods
    GET /api/v1/foods, returns all foods currently in the database.

--- index.js
+++ index.js
@@ -15,7 +15,15 @@ app.get('/', (request, response) => {
   response.send('Hello');
 });
 
-
+app.get('/api/v1/foods', (request, response) => {
+  database('foods').select()
+    .then((foods) => {
+      response.status(200).json(foods);
+    })
+    .catch((error) => {
+      response.status(500).json({ error });
+    });
+});
 
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on ${app.get('port')}.`);
