5fe0a4a1f95fb169d7d942eeccf3d86bdeaf8e15
Preston Jarnagin <prstnjarnagin@gmail.com>
2019-01-31 13:56:31 -0700

    Remove redundancy

--- knexfile.js
+++ knexfile.js
@@ -18,15 +18,13 @@ module.exports = {
     client: 'pg',
     connection: process.env.DATABASE_URL + `?ssl=true`,
     migrations: {
-      directory: './db/migrations'
+      directory: './db/migrations',
+      tableName: 'knex_migrations'
     },
     useNullAsDefault: true,
     pool: {
       min: 2,
       max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
     }
   }
 
