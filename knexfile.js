1e919cee67526537447d5e70c08799600dbc38fc
Preston Jarnagin <prstnjarnagin@gmail.com>
2019-02-06 16:46:24 -0700

    change travis config

--- knexfile.js
+++ knexfile.js
@@ -2,42 +2,32 @@
 
 module.exports = {
 
-  // development: {
-  //   client: 'sqlite3',
-  //   connection: {
-  //     filename: './dev.sqlite3'
-  //   }
-  // },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+  development: {
+      client: 'pg',
+      connection: 'postgres://localhost/calorie_tracker',
+      migrations: {
+        directory: './db/migrations'
+      },
+      seeds: {
+        directory: './db/seeds/dev'
+      },
+      useNullAsDefault: true
     },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
 
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+    client: 'pg',
+    connection: process.env.DATABASE_URL + `?ssl=true`,
+    migrations: {
+      directory: './db/migrations',
+      tableName: 'knex_migrations'
     },
+    seeds: {
+      directory: './db/seeds/production'
+    },
+    useNullAsDefault: true,
     pool: {
       min: 2,
       max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
     }
   }
 
